6ad2d196907beb344fff387d7706154f3e40b195
InnoFang <innofang@outlook.com>
2021-06-21 21:54:58 +0800

    style: clean code

--- include/t_balanced_clustering.h
+++ include/t_balanced_clustering.h
@@ -25,36 +25,25 @@ struct ClusterCenter {
 
 class TBC {
 public:
-    TBC(const std::vector<std::vector<double>> &data, int clusterNum, unsigned int seed = std::random_device()());
-
+    TBC(const std::vector<std::vector<double>> &data, int k, int tau, unsigned int seed = std::random_device()());
     ~TBC();
-
     double start();
-
     void initClusterCenters();
-
-    void initClusters();
-
     void updateClusterCenters();
-
     void reassign();
-
     void assignToNearestCenter(int dataIndex, std::vector<double> &center);
-
     double distance(const std::vector<double> &data1, const std::vector<double> &data2) const;
-
     std::vector<int> getAssignments();
 
 private:
-    std::vector<std::vector<double>> data_;              // 原始数据
-//    std::vector<std::vector<double>> clusterCenters;    // 簇中心的数据
-    std::vector<ClusterCenter> clusterCenters;
-    std::vector<int> assignments;                    // 每条数据预测的类别
+    std::vector<std::vector<double>> data_;       // 原始数据
+    std::vector<ClusterCenter> clusterCenters;    // 簇中心的数据
+    std::vector<int> assignments;                 // 每条数据预测的类别
     int k_;                                       // k个簇中心
-    size_t size_;                                        // 数据数量
-    size_t dims_;                                        // 数据维度
-    size_t scope_;                                          // 每个簇的数据量有多少
-    std::default_random_engine randomEngine;            // 随机引擎
+    size_t size_;                                 // 数据数量
+    size_t dims_;                                 // 数据维度
+    size_t bound_;                                // 每个簇的数据量有多少
+    std::default_random_engine randomEngine;      // 随机引擎
 };
 
 #endif //TBC__T_BALANCED_CLUSTERING_H
