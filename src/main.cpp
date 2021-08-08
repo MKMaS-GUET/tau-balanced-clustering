2c82f837b17ff115b358235f59d6d4c4490f1c7e
InnoFang <innofang@outlook.com>
2021-08-08 11:44:26 +0800

    pref: clean code

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include <map>
 #include <vector>
 #include <chrono>
 #include <sstream>
@@ -6,18 +5,10 @@
 #include <iostream>
 #include "tau_balanced_clustering.hpp"
 
-static const auto io_speed_up = [] {
-    std::stringstream::sync_with_stdio(false);
-    std::fstream::sync_with_stdio(false);
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    return 0;
-} ();
-
 std::vector<std::vector<double>> readCSV(const std::string &filepath) {
     std::vector<std::vector<double>> data;
     std::ifstream reader(filepath);
+    std::ifstream::sync_with_stdio(false);
     std::string line;
     while (reader >> line) {
         std::stringstream raw(line);
@@ -39,6 +30,10 @@ void writeAssignment(const std::string &filepath, const std::vector<int> &assign
 }
 
 int main(int argc, char *argv[]) {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
     /* ' ./tbc <input> <output> <k> <tau> [seed] ' */
     if (argc < 5 || argc > 6) {
         std::cerr << "./tbc <input> <output> <k> <tau> [seed]" << std::endl;
@@ -58,20 +53,19 @@ int main(int argc, char *argv[]) {
         exit(0);
     }
 
-    TBC *tbc = new TBC(data, cluster, tau, seed);
+    TBC tbc(data, cluster, tau, seed);
 
     auto start_time = std::chrono::high_resolution_clock::now();
 
-    tbc->start();
+    tbc.start();
 
     double used_time = std::chrono::duration_cast<std::chrono::duration<double>>(
             std::chrono::high_resolution_clock::now() - start_time)
             .count();
 
-    writeAssignment(outputPath, tbc->getAssignments());
+    writeAssignment(outputPath, tbc.getAssignments());
 
     std::cout << filepath << " , used time: " << used_time << std::endl;
 
-    delete tbc;
     return 0;
 }
